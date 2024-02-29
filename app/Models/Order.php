8606581490f390d7e25b4640eb244a34fbceba3d
ParraJuanPablo <parrajuanpalo@gmail.com>
2024-02-29 11:43:54 -0400

    #5 Order Fixed

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Order extends Model
 {
@@ -21,8 +22,8 @@ class Order extends Model
         return $this->hasMany(Payment::class);
     }
 
-    public function receipts(): HasMany
+    public function receipts(): BelongsTo
     {
-        return $this->hasMany(Receipt::class);
+        return $this->belongsTo(Receipt::class);
     }
 }
