eb471cefa946c41b5e4a25d209c788aea52507f9
ParraJuanPablo <parrajuanpalo@gmail.com>
2024-03-05 10:21:39 -0400

    #22 Verify Email Created

--- resources/js/Pages/Auth/VerifyEmail.jsx
+++ resources/js/Pages/Auth/VerifyEmail.jsx
@@ -1,6 +1,8 @@
-import GuestLayout from '@/Layouts/GuestLayout';
-import PrimaryButton from '@/Components/PrimaryButton';
-import { Head, Link, useForm } from '@inertiajs/react';
+import GuestLayout from "@/Layouts/GuestLayout";
+import PrimaryButton from "@/Components/PrimaryButton";
+import { Head, Link, useForm } from "@inertiajs/react";
+import ApplicationLogo from "@/Components/ApplicationLogo";
+import Footer from "@/Components/Navigation/Footer";
 
 export default function VerifyEmail({ status }) {
     const { post, processing } = useForm({});
@@ -8,38 +10,61 @@ export default function VerifyEmail({ status }) {
     const submit = (e) => {
         e.preventDefault();
 
-        post(route('verification.send'));
+        post(route("verification.send"));
     };
 
     return (
-        <GuestLayout>
+        <>
             <Head title="Email Verification" />
 
-            <div className="mb-4 text-sm text-gray-600">
-                Thanks for signing up! Before getting started, could you verify your email address by clicking on the
-                link we just emailed to you? If you didn't receive the email, we will gladly send you another.
-            </div>
-
-            {status === 'verification-link-sent' && (
+            {status === "verification-link-sent" && (
                 <div className="mb-4 font-medium text-sm text-green-600">
-                    A new verification link has been sent to the email address you provided during registration.
+                    A new verification link has been sent to the email address
+                    you provided during registration.
                 </div>
             )}
-
-            <form onSubmit={submit}>
-                <div className="mt-4 flex items-center justify-between">
-                    <PrimaryButton disabled={processing}>Resend Verification Email</PrimaryButton>
-
-                    <Link
-                        href={route('logout')}
-                        method="post"
-                        as="button"
-                        className="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                    >
-                        Log Out
-                    </Link>
+            <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mb-20">
+                <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+                    <div className="flex justify-center ">
+                        <div className="h-20">
+                            <ApplicationLogo />
+                        </div>
+                    </div>
+                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-primary">
+                        Verify your email
+                    </h2>
+                    <div className="mb-4 text-sm text-gray-600 mt-4 text-center">
+                        Thanks for signing up! Before getting started, could you
+                        verify your email address by clicking on the link we
+                        just emailed to you? If you didn't receive the email, we
+                        will gladly send you another.
+                    </div>
                 </div>
-            </form>
-        </GuestLayout>
+
+                <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+                    <form onSubmit={submit}>
+                        <div className="mt-4 flex">
+                            <div className="flex items-center justify-center mt-8 w-full h-12">
+                                <PrimaryButton
+                                    className="w-full h-8 justify-center"
+                                    disabled={processing}
+                                >
+                                    Register
+                                </PrimaryButton>
+                            </div>
+                        </div>
+                        <Link
+                            href={route("logout")}
+                            method="post"
+                            as="button"
+                            className="underline flex items-center justify-center mt-4 w-full text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Log Out
+                        </Link>
+                    </form>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
